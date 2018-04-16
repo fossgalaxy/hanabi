6ee9512d0522a26013aa13c19f9d9a7668ffce2a
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2018-04-16 16:20:03 +0100

    add comments to canfire method

--- src/main/java/com/fossgalaxy/games/fireworks/ai/rule/Rule.java
+++ src/main/java/com/fossgalaxy/games/fireworks/ai/rule/Rule.java
@@ -10,6 +10,15 @@ import com.fossgalaxy.games.fireworks.state.actions.Action;
 @FunctionalInterface
 public interface Rule {
 
+    /**
+     * return true if the rule should execute.
+     *
+     * If this returns true, the rule will be selected for execution. The rule MUST NOT return null if this is the case.
+     *
+     * @param playerID the playerID of the current agent
+     * @param state the current game state
+     * @return true if this rule should be selected, false otherwise.
+     */
     default boolean canFire(int playerID, GameState state) {
         Action returnedAction = execute(playerID, state);
         return returnedAction != null;
@@ -19,7 +28,7 @@ public interface Rule {
      * Return true if it is possible this rule could fire, and return false if you can guarantee that it cannot fire.
      * @param playerID The player id that is this turn
      * @param state The state of the board
-     * @return wether we could fire for that player in this state
+     * @return false if we should skip this rule, true if we should consult canFire
      */
     default boolean couldFire(int playerID, GameState state){
         return true;
